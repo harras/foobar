b5c1dcbb61939f62b466d67c333463f335346013
Nicholas Harras <nickharras1@gmail.com>
2018-03-02 18:36:34 -0500

    First commit, adding answer to Level 1

---
+++ Level 1/README.md
@@ -0,0 +1,2 @@
+This program creates a string of all primes concatenated together up to a 
+length of n+5, then returns a 5 digit substring beginning at the nth index.
---
+++ Level 1/solution.py
@@ -0,0 +1,23 @@
+def answer(n):
+	primes = [2] 	# Initialize a list of primes with just 2
+	primes_str = "2"
+	num = 3			# Start iterating from 3
+	while len(primes_str) < n+5:
+		isPrime = True
+		for i in primes:
+			if num%i == 0:
+				isPrime = False
+				break
+			elif i**2 > num: 	# No need to keep looking for factors 
+				break			# past sqrt(num)
+		if isPrime:
+			primes.append(num)
+			primes_str += str(num)
+		num += 1
+
+	return primes_str[n:n+5]
+
+
+
+
+print answer(3)
